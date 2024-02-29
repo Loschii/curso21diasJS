a73f53395aad15d4e28e4800d2dd5a674096f485
Loschii <bruno.loschi@outlook.com>
2024-02-29 15:07:08 -0300

    Aula 3

---
+++ dia-3/dia3.js
@@ -0,0 +1,18 @@
+// let nome = prompt('Digite seu nome');
+// let idade = parseInt(prompt('Digite sua idade'));
+// let altura = parseFloat(prompt('Digite sua altura'));
+// let peso = Number(prompt('Digite o seu peso'));
+// let anoNascimento = 2023 - idade;
+// let imc = peso / (altura * altura);
+
+// console.log(`Olá ${nome}, você tem ${idade} anos, nasceu em ${anoNascimento}, tem ${altura} de altura, pesa ${peso} kg seu IMC ${imc} é Kg/m2`)
+
+
+
+let valor = "20"
+let texto = "R$"
+
+valor = valor + 5
+valor = Number(valor)
+
+console.log('O valor do seu prato é de ' + texto + valor)
